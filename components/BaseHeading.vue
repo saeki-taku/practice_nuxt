ebab686e95217fe984fa1a33a3960b989df973d4
Taku <63911366+saeki-taku@users.noreply.github.com>
2020-11-28 13:25:40 +0900

    test

---
+++ components/BaseHeading.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2 class="font-sans text-lg text-gray-800 text-center text-3xl mb-10">
+    <slot />
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeading',
+}
+</script>
+
+<style scoped>
+h2 {
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
